2a5df77eec96648388c46d11343f9596de124f90
kumar <imkumarb@gmail.com>
2023-01-07 12:45:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class User{
+
+    constructor(){
+        this.message = 'Hello World';
+        console.log(this.message);
+    }
+
+    getUserName(){}
+
+    get UserStatus(){}
+
+
+}
+
+module.exports = Us
